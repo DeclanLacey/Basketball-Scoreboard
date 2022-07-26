beb81062894377a5a7bdaf6044b1f74c6054a080
DeclanLacey <109444336+DeclanLacey@users.noreply.github.com>
2022-07-26 09:42:11 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+
+let homeStoreEl = document.getElementById("home-score")
+let homeScore = 0 
+
+function increaseHomeScoreOne() {
+    
+    
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+    
+}
+
+function increaseHomeScoreTwo() {
+    
+    homeScore += 2
+    homeStoreEl.textContent = homeScore
+    
+    
+}
+
+function increaseHomeScoreThree() {
+    
+    homeScore += 3
+    homeStoreEl.textContent = homeScore
+    
+    
+}
+
+
+
+
+
+let awayStoreEl = document.getElementById("away-score")
+let awayScore = 0
+
+function increaseAwayScoreOne() {
+    
+    awayScore += 1
+    awayStoreEl.textContent = awayScore    
+}
+
+function increaseAwayScoreTwo() {
+    
+   awayScore += 2 
+   awayStoreEl.textContent = awayScore
+    
+}
+
+
+function increaseAwayScoreThree() {
+    
+    awayScore += 3
+    awayStoreEl.textContent = awayScore
+    
+}
